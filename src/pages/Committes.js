8d4286ad124a093480225d8cea06c24afb441414
Mario Colosi <colosimario96@gmail.com>
2023-03-30 08:39:44 +0200

    update: new publicity chair added

--- src/pages/Committes.js
+++ src/pages/Committes.js
@@ -57,6 +57,9 @@ const CallForPaper = () => {
                         <tr>
                             <td>Dip Sankar Banerjee, Indian Institute of Technology (IIT) Jodhpur, India</td>
                         </tr>
+                        <tr>
+                            <td>Carlos Westphall, Federal University of Santa Catarina, Brazil</td>
+                        </tr>
 
                         <tr>
                             <th>
