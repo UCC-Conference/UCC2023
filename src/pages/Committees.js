f4effdeb297485a9c8b6ff63b33428a22cb1a6ac
Lorenzo Carnevale <lcarnevale@unime.it>
2023-04-05 16:55:27 +0200

    add Beilei Jiang as Poster Co-Chair

--- src/pages/Committees.js
+++ src/pages/Committees.js
@@ -45,6 +45,15 @@ const Committees = () => {
     
                         <tr>
                             <th>
+                                <h3>Poster Co-Chair(s)</h3>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>Beilei Jiang, University of North Texas, USA</td>
+                        </tr>
+    
+                        <tr>
+                            <th>
                                 <h3>Publicity Co-Chair(s)</h3>
                             </th>
                         </tr>
