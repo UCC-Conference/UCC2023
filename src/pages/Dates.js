14db2dd076702bb05e96fc4ae0dbfa5aae28b304
Lorenzo Carnevale <lcarnevale@unime.it>
2023-04-21 13:59:43 +0200

    anticipate dates of about 10 days

--- src/pages/Dates.js
+++ src/pages/Dates.js
@@ -9,11 +9,11 @@ const Dates = () => {
                 <h1>Important Dates</h1>
                 <hr />
                 <p><strong>Timezone</strong><br/>Anywhere in the world!</p>
-                <p><strong>Paper Submissions Due</strong><br/>August 13, 2023</p>
+                <p><strong>Paper Submissions Due</strong><br/>August 3, 2023</p>
 
-                <p><strong>Acceptance Notification</strong><br/>October 9, 2023</p>
+                <p><strong>Acceptance Notification</strong><br/>September 30, 2023</p>
 
-                <p><strong>Camera Ready Papers Due</strong><br/>October 31, 2023</p>
+                <p><strong>Camera Ready Papers Due</strong><br/>October 21, 2023</p>
             </Content>
         </PageWrapper>
     );
