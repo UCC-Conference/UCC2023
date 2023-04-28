15892df9d35a7d1e3c1f4f1d86f60c4c500ad242
Lorenzo Carnevale <lcarnevale@unime.it>
2023-04-28 09:32:06 +0200

    update co-chairs fields

--- src/pages/Committees.js
+++ src/pages/Committees.js
@@ -96,7 +96,7 @@ const Committees = () => {
 
                         <tr>
                             <th>
-                                <h3>Student Program Chair(s)</h3>
+                                <h3>Student Program (Co-)Chair(s)</h3>
                             </th>
                         </tr>
                         <tr>
@@ -108,7 +108,7 @@ const Committees = () => {
 
                         <tr>
                             <th>
-                                <h3>Proceedings Chair(s)</h3>
+                                <h3>Proceedings (Co-)Chair(s)</h3>
                             </th>
                         </tr>
                         <tr>
