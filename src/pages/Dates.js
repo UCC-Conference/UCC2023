7e5c90a22ab474573f75654a467e30501b907516
Lorenzo Carnevale <lorenzocarnevale@gmail.com>
2023-05-16 09:12:18 +0200

    update with workshops date

--- src/pages/Dates.js
+++ src/pages/Dates.js
@@ -9,11 +9,12 @@ const Dates = () => {
                 <h1>Important Dates</h1>
                 <hr />
                 <p><strong>Timezone</strong><br/>Anywhere in the world!</p>
-                <p><strong>Paper Submissions Due</strong><br/>August 3, 2023</p>
-
-                <p><strong>Acceptance Notification</strong><br/>September 30, 2023</p>
-
-                <p><strong>Camera Ready Papers Due</strong><br/>October 21, 2023</p>
+                <p><strong>Workshop Proposals Due</strong><br/>May 31, 2023</p>
+                <p><strong>Workshop Proposals - Notification of Acceptance</strong><br/>June 15, 2023</p>
+                <p><strong>URL of Workshop</strong><br/>June 25, 2023</p>
+                <p><strong>Conference Paper Submissions Due</strong><br/>August 3, 2023</p>
+                <p><strong>Conference Paper - Notification of Acceptance</strong><br/>September 30, 2023</p>
+                <p><strong>Camera-Ready Due</strong><br/>October 21, 2023</p>
             </Content>
         </PageWrapper>
     );
