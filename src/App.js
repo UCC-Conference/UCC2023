9d8471cde7e4c90c4683496f9f556400aa6e696c
Mario Colosi <colosimario96@gmail.com>
2023-05-21 18:11:55 +0200

    histats added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { Route, Routes as BaseRoutes, useLocation, useNavigate } from "react-router-dom";
-import { Helmet } from 'react-helmet';
 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
@@ -30,21 +29,6 @@ function App() {
                 </BaseRoutes>
             </main>
             <Footer />
-            <Helmet>
-                <script>
-                    {`
-                        var _Hasync= _Hasync|| [];
-                        _Hasync.push(['Histats.start', '1,4767673,4,0,0,0,00010000']);
-                        _Hasync.push(['Histats.fasi', '1']);
-                        _Hasync.push(['Histats.track_hits', '']);
-                        (function() {
-                        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-                        hs.src = ('//s10.histats.com/js15_as.js');
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-                        })();
-                    `}
-                </script>
-            </Helmet>
         </>
     );
 }
