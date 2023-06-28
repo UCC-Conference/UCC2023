e6e157ec08e68f9a014687b23ad30d2c8312d7ae
Mario Colosi <colosimario96@gmail.com>
2023-06-28 09:19:22 +0200

    update: workshop proposals dates updated

--- src/pages/Dates.js
+++ src/pages/Dates.js
@@ -9,9 +9,9 @@ const Dates = () => {
                 <h1>Important Dates</h1>
                 <hr />
                 <p><strong>Timezone</strong><br/>Anywhere in the world!</p>
-                <p><strong>Workshop Proposals Due</strong><br/>June 14, 2023</p>
-                <p><strong>Workshop Proposals - Notification of Acceptance</strong><br/>June 15, 2023</p>
-                <p><strong>URL of Workshop</strong><br/>June 25, 2023</p>
+                <p><del><strong>Workshop Proposals Due</strong><br/>June 14, 2023</del></p>
+                <p><del><strong>Workshop Proposals - Notification of Acceptance</strong><br/>June 15, 2023</del></p>
+                <p><strong>URL of Workshop</strong><br/>July 7, 2023</p>
                 <p><strong>Conference Paper Submissions Due</strong><br/>August 3, 2023</p>
                 <p><strong>Conference Paper - Notification of Acceptance</strong><br/>September 30, 2023</p>
                 <p><strong>Camera-Ready Due</strong><br/>October 21, 2023</p>
